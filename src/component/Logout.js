efcec51872a89fc0e76200000feaf1f62ea76c59
Vincent Vargas <vincentvargas6969@gmail.com>
2023-04-04 05:00:41 -0800

    Updates

--- src/component/Logout.js
+++
@@ -1,6 +0,0 @@
-function handleLogout() {
-    localStorage.removeItem('isAuthenticated');
-    window.location.reload(); // Refresh the page to clear state
-}
-
-export default handleLogout;
--- src/component/LogoutFunction.js
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import handleLogout from './Logout';
-
-function Logout() {
-    handleLogout();
-    return <Navigate to="/login" />;
-}
-
-export default Logout
